c567425f82f667e9ff99a0f3ec8be9936014c56a
Tim Lampaert <tim.lampaert@telenet.be>
2018-12-09 22:55:59 +0100

    Part 1

--- public/script.js
+++ public/script.js
@@ -1,1 +1,56 @@
-console.log('It works.');
+new Vue({
+    el:"#app",
+    data: {
+        total: 0,
+        items: [],
+        cart: [],
+        searchValue: 'baby',
+        lastSearch:'',
+        loading:false,
+        price: 9.99
+    },
+    methods: {
+        addItem: function(index){
+            this.total += this.items[index].unitPrice;
+            if(this.items[index].quantity == 0){
+                this.cart.push(this.items[index]);
+            }
+            this.items[index].quantity ++
+        },
+        inc: function(item)
+        {
+            item.quantity ++;
+            this.total += 9.99;
+        },
+        dec: function(item)
+        {
+            item.quantity --;
+            this.total -=9.99;
+            if(item.quantity == 0)
+            {
+                this.cart.splice(this.cart[this.cart.indexOf(item)], 1);
+            }
+        },
+        searchSubmit: function()
+        {
+            this.loading = true;
+            items = [];
+            this.$http.get('/search/'.concat(this.searchValue))
+                      .then(function(res){ 
+                            this.lastSearch = this.searchValue;
+                            this.items = res.data;
+                            this.loading = false;
+                       });
+        }
+    },
+    filters: 
+    {
+        currency: function(price)
+        {
+            return '$'.concat(price.toFixed(2));
+        }
+    },
+    mounted: function(){
+        this.searchSubmit();
+    }
+});
